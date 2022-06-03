f1b09a66721e6f2f11d581b1b0b8e30cc9a39694
Arturo Diaz <74999834+aru-diaz@users.noreply.github.com>
2022-06-03 16:01:37 -0700

    make it better2

--- universidad/settings.py
+++ universidad/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gag2frc$s^7khj-ax^78sz#xiypa&3nbk%p8!_)3#%45hk3*x!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
