27cef7208046f7d75dd56f4c252e5fbbec32e058
Arturo Diaz <arturo.diabra97@gmail.com>
2022-06-02 23:36:12 -0700

    Creacion edicion y eliminacion grupo

--- Apps/Horario/views.py
+++ Apps/Horario/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Materia, Docente, Grupo
 from django.db import IntegrityError
+from django.contrib import messages
 
 # Create your views here.
 def inicio(request):    
@@ -17,8 +18,10 @@ def registrarMateria(request):
     try:
         materia = Materia.objects.create(
             idMateria=idMateria, nombreMateria=nombreMateria, creditosMateria=creditosMateria)
+        messages.success(request, '¡Materia registrada!')
     except IntegrityError:
-        return redirect('/materias')
+        messages.success(request, '¡Materia NO registrada, duplicado de id!')
+        return redirect('/materias')        
 
     return redirect('/materias')
 
@@ -26,6 +29,7 @@ def eliminarMateria(request, idMateria):
     materia = Materia.objects.get(idMateria=idMateria)
     materia.delete()
 
+    messages.success(request, '¡Materia eliminada!')
     return redirect('/materias')
 
 def edicionMateria(request, idMateria):
@@ -36,12 +40,15 @@ def editarMateria(request):
     idMateria = request.POST['idMateria']
     nombreMateria = request.POST['nombreMateria']
     creditosMateria = request.POST['creditosMateria']
-
-    materia = Materia.objects.get(idMateria=idMateria)
-    materia.nombreMateria = nombreMateria
-    materia.creditosMateria = creditosMateria
-    materia.save()
-
+    try:
+        materia = Materia.objects.get(idMateria=idMateria)
+        materia.nombreMateria = nombreMateria
+        materia.creditosMateria = creditosMateria
+        materia.save()
+        messages.success(request, '¡Materia actualizada!')
+    except IntegrityError:
+        messages.success(request, '¡La materia no se pudo editar!')
+        return redirect('/materias')     
     return redirect('/materias')
 
 ##################################################################
@@ -58,14 +65,16 @@ def registrarDocente(request):
     try:
         docente = Docente.objects.create(
             idDocente=idDocente, nombreDocente=nombreDocente, apellidoDocente=apellidoDocente)
+        messages.success(request, '¡Docente registrado!')
     except IntegrityError:
+        messages.success(request, '¡Docente NO registrado, duplicado de id!')
         return redirect('/docentes')
     return redirect('/docentes')
 
 def eliminarDocente(request, idDocente):
     docente = Docente.objects.get(idDocente=idDocente)
     docente.delete()
-
+    messages.success(request, '¡Docente eliminado!')
     return redirect('/docentes')
 
 def edicionDocente(request, idDocente):
@@ -76,11 +85,15 @@ def editarDocente(request):
     idDocente = request.POST['idDocente']
     nombreDocente = request.POST['nombreDocente']
     apellidoDocente = request.POST['apellidoDocente']
-
-    docente = Docente.objects.get(idDocente=idDocente)
-    docente.nombreDocente = nombreDocente
-    docente.apellidoDocente = apellidoDocente
-    docente.save()
+    try:
+        docente = Docente.objects.get(idDocente=idDocente)
+        docente.nombreDocente = nombreDocente
+        docente.apellidoDocente = apellidoDocente
+        docente.save()
+        messages.success(request, '¡Docente actualizado!')
+    except IntegrityError:
+        messages.success(request, '¡El docente no se pudo editar!')
+        return redirect('/docentes')         
 
     return redirect('/docentes')
 
@@ -97,29 +110,37 @@ def registrarGrupo(request):
 
     try:
         grupo = Grupo.objects.create(
-            idGrupo=idGrupo, nombreGrupo=nombreGrupo, idDocente=idDocente)
+            idGrupo=idGrupo, nombreGrupo=nombreGrupo, idDocente=Docente.objects.get(idDocente=idDocente))
+        messages.success(request, '¡Grupo registrado!')
     except IntegrityError:
+        messages.success(request, '¡Grupo NO registrado!, duplicado de id o duplicado de docente')
         return redirect('/grupos')
     return redirect('/grupos')
 
 def eliminarGrupo(request, idGrupo):
     docente = Grupo.objects.get(idGrupo=idGrupo)
     docente.delete()
-
+    messages.success(request, '¡Grupo eliminado!')
     return redirect('/grupos')
 
 def edicionGrupo(request, idGrupo):
-    docente = Grupo.objects.get(idGrupo=idGrupo)
-    return render(request, "grupos/edicionGrupos.html", {"docente": docente})
+    grupo = Grupo.objects.get(idGrupo=idGrupo)
+    docentes = Docente.objects.all()
+    return render(request, "grupos/edicionGrupos.html", {"grupo": grupo, "docentes":docentes})
 
 def editarGrupo(request):
     idGrupo = request.POST['idGrupo']
     nombreGrupo = request.POST['nombreGrupo']
-    apellidoGrupo = request.POST['apellidoGrupo']
+    idDocente = request.POST['idDocente']
 
-    docente = Grupo.objects.get(idGrupo=idGrupo)
-    docente.nombreGrupo = nombreGrupo
-    docente.apellidoGrupo = apellidoGrupo
-    docente.save()
+    try:
+        grupo = Grupo.objects.get(idGrupo=idGrupo)
+        grupo.nombreGrupo = nombreGrupo
+        grupo.idDocente = Docente.objects.get(idDocente=idDocente)
+        grupo.save()
+        messages.success(request, '¡Grupo actualizado!')
+    except IntegrityError:
+        messages.success(request, '¡El grupo no se pudo editar!')
+        return redirect('/grupos')          
 
     return redirect('/grupos')
